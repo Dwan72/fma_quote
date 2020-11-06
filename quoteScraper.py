6eed11b01d537e32c1f82f4ea69924bb42b6d1e1
lil-Dwan <alex.cap.an@gmail.com>
2020-11-06 11:43:13 -0600

    changing python script

--- quoteScraper.py
+++ quoteScraper.py
@@ -8,8 +8,30 @@ if not sys.argv[1]:
 
 
 file_name = sys.argv[1]
+
+
 file_obj = open(file_name, mode='r')
+file_output = open("organized_quote.txt", mode='w')
+
+
 print(file_obj)
 
-for line in file_obj:
-    print(line)
+
+
+# for line in file_obj:
+#     if line[0] == "(":
+#         continue
+
+#     elif line[0] == '"':
+#         continue
+
+
+testArray = []
+testObj = {1, "quote1", "alphonse"} 
+testObj2 = {2, "quote2", "joseph"}
+
+testArray.append(testObj)
+testArray.append(testObj2)
+
+print(testArray)
+
