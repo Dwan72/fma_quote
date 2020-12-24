f2b6508c511b79e3aef97886567ac48d3077fb09
Dwan72 <alex.cap.an@gmail.com>
2020-12-24 00:07:09 -0600

    adding fiels

---
+++ backup_front/src/components/Home.js
@@ -0,0 +1,15 @@
+import '../styles/Home.css'
+import Container from 'react-bootstrap/Container';
+
+
+function Home() {
+
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default Home;
---
+++ backup_front/src/components/Lab.js
@@ -0,0 +1,10 @@
+function Lab() {
+    return (
+        <div>
+            <h1>This is Lab</h1>
+        </div>
+    );
+}
+  
+  export default Lab;
+  
