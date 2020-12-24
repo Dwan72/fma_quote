3f4ce14f942fa96e4c738db2c7dd7f7b035e5380
Dwan72 <alex.cap.an@gmail.com>
2020-12-24 00:15:27 -0600

    adding front

---
+++ random_quote_front/src/components/Documentation.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+
+
+
+function Documentation() {
+    return (
+        <Container>
+
+            <h1>This is Documentation</h1>
+        </Container>
+    );
+}
+
+export default Documentation;
