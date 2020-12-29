978c0e8735da101ba0ddd20dad14b8ba81de7b5e
ShannenB <sbarrameda17@gmail.com>
2020-12-29 00:11:23 -0600

    created character json and added card deck to home

---
+++ random_quote_front/src/characters.js
@@ -0,0 +1,102 @@
+export const characters = [
+    {
+        name: "Edward Elric", 
+        id: "1"
+    },
+    {
+        name: "Alphonse Elric",
+        id: "2"
+    },
+    {
+        name: "Roy Mustang",
+        id: "3"
+    },
+    {
+        name: "Riza Hawkeye",
+        id: "4"
+    },
+    {
+        name: "Winry Rockbell",
+        id: "5"
+    },
+    {
+        name: "Maes Hughes",
+        id: "6"
+    },
+    {
+        name: "Izumi Curtis",
+        id: "7"
+    },
+    {
+        name: "Solf J. Kimblee",
+        id: "8"
+    },
+    {
+        name: "Van Hohenheim",
+        id: "9"
+    },
+    {
+        name: "Trisha Elric",
+        id: "10"
+    },
+    {
+        name: "Ling Yao",
+        id: "11"
+    },
+    {
+        name: "Fu",
+        id: "12"
+    },
+    {
+        name: "Lan Fan",
+        id: "13"
+    },
+    {
+        name: "Olivier Mira Armstrong",
+        id: "14"
+    },
+    {
+        name: "Dr. Knox",
+        id: "15"
+    },
+    {
+        name: "Tim Marcoh",
+        id: "16"
+    },
+    {
+        name: "Lust",
+        id: "17"
+    },
+    {
+        name: "Greed",
+        id: "18"
+    },
+    {
+        name: "Envy",
+        id: "19"
+    },
+    {
+        name: "Gluttony",
+        id: "20"
+    },
+    {
+        name: "Pride",
+        id: "21"
+    },
+    {
+        name: "Wrath",
+        id: "22"
+    },
+    {
+        name: "Father",
+        id: "23"
+    },
+    {
+        name: "Truth",
+        id: "24"
+    },
+    {
+        name: "Major Miles",
+        id: "25"
+    }
+];
--- random_quote_front/src/components/Home.js
+++ random_quote_front/src/components/Home.js
@@ -1,14 +1,41 @@
 import '../styles/Home.css'
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
+import { characters } from '../characters.js';
 
 function Home() {
-
-
+    var characterList = characters.map(
+        function(character){
+            return (
+                <Col className="container-fluid mt-4">
+                    <Card style={{ width: '20rem' }}> 
+                        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                        <Card.Body>
+                            <Card.Title className="">{character.name}</Card.Title>
+                            <Card.Text>
+                                {character.id}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <small className="text-muted"></small>
+                        </Card.Footer>
+                    </Card> 
+                </Col>
+            )
+        }
+    )
+    
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Container>
+            <CardDeck>
+                {characterList}
+            </CardDeck>
+        </Container>
     );
 }
 
